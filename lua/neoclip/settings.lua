bf8a6a721e4d7cb2e6b35f61ed5f24cae258dd2a
Chaz Leong <13462818+cleong14@users.noreply.github.com>
2024-05-17 22:09:10 -1000

    fix: remove deprecated code
    Signed-off-by: Chaz Leong <13462818+cleong14@users.noreply.github.com>

--- lua/neoclip/settings.lua
+++ lua/neoclip/settings.lua
@@ -92,7 +92,7 @@ local function check_deprecated_entries()
 end
 
 local function is_dict_like (tbl)
-    return type(tbl) == 'table' and not vim.tbl_islist(tbl)
+    return type(tbl) == 'table' and not vim.islist(tbl)
 end
 
 local function setup(opts, subsettings)
